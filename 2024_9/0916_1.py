bf70b48cf6bf48594e4d2be97c21a96a8b4bd67d
柴崎優 <berght3@gmail.com>
2024-09-18 21:53:19 +0800

    0918

--- 2024_9/0916_1.py
+++ 2024_9/0916_1.py
@@ -1,8 +1,33 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+# nums = list(map(int,input().split()))
+# value = int(input())
+
+nums = [1,2,3,2,3,2]
+value = 2
+count = 0
+
+for i in range(0,len(nums)):
+    if nums[i] == value:
+        nums[i] = "_"
+        count += 1
+        
+print(nums)
+
+temp_nums = []
+temp_nums_ = []
+for i in nums:
+    if i == "_":
+        temp_nums_.append(i)
+    else:
+        temp_nums.append(i)
+        
+print(temp_nums,temp_nums_)
+
+        
+temp_nums.sort()
+temp_nums.extend(temp_nums_) #その関数がなにを返すのか理解する
+print(temp_nums)
+        
+
+        
+        
+        
