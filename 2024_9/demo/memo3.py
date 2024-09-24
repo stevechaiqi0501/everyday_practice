72bd6de9a6417f9126b70e829bda637af82a0f7e
柴崎優 <berght3@gmail.com>
2024-09-24 15:05:07 +0800

    0924_2

--- 2024_9/demo/memo3.py
+++ 2024_9/demo/memo3.py
@@ -4,7 +4,7 @@
 # Example 2:
 
 # x = int(input)
-x = 333
+x = 321
 
 if x<0:
     print("False")
@@ -14,6 +14,9 @@ else:
     for i in range(len(x_str)):
         digit = x % 10
         int_num = int_num*10 + digit 
-        digit = digit//10
-        print(int_num)
-print(int_num)
+        x = x//10
+        
+if int_num == x:
+    print(True)
+else:
+    print(False)
